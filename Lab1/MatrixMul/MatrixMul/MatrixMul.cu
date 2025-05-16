fa8bf603be3d23e17f90d4936d8cabac5e3b088a
KNS121 <komaroff.ns@gmail.com>
2025-05-16 18:24:40 +0300

    generate matrix

---
+++ Lab1/MatrixMul/MatrixMul/MatrixMul.cu
@@ -0,0 +1,39 @@
+﻿#include "MatrixMulCPU.h"
+#include <vector>
+#include <stdexcept>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+using std::vector;
+
+#define N 5
+
+void fillMatrix(vector<vector<int>>& matrix) {
+    for (auto& row : matrix) {
+        for (auto& element : row) {
+            element = rand() % 100;
+        }
+    }
+}
+
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (const auto& element : row) {
+            std::cout << element << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+
+    vector<vector<int>> A(N, vector<int>(N));
+
+    fillMatrix(A);
+
+    printMatrix(A);
+
+    return 0;
+}
---
+++ Lab1/MatrixMul/MatrixMul/MatrixMulCPU.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#ifndef MATRIXMULCPU_H
+#define MATRIXMULCPU_H
+
+#include <vector>
+using std::vector;
+
+vector<vector<int>> MatrixMultiplyCPU(const vector<vector<int>>& A,
+    const vector<vector<int>>& B, const int N);
+
+#endif
