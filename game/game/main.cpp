b0dede162af3a419281216207d466b4ffe74b660
KNS121 <komaroff.ns@gmail.com>
2025-05-17 23:05:39 +0300

    pochinil uprugost...

--- game/game/main.cpp
+++ game/game/main.cpp
@@ -8,19 +8,20 @@ struct Particle {
     int type;
 };
 
-const int NUM_PARTICLES = 5;
+const int NUM_PARTICLES = 8;
 std::vector<Particle> particles;
+
 
 
 // init chasticy
 void initParticles() {
     particles.resize(NUM_PARTICLES);
     for (auto& p : particles) {
- 
+
         p.x = (rand() % 2000 - 1000) / 1000.0f;
         p.y = (rand() % 2000 - 1000) / 1000.0f;
-        p.vx = (rand() % 100 - 50) / 500.0f;
-        p.vy = (rand() % 100 - 50) / 500.0f;
+        p.vx = (rand() % 100 - 100) / 10000.0f;
+        p.vy = (rand() % 100 - 100) / 10000.0f;
         p.type = rand() % 2;
 
     }
@@ -29,18 +30,18 @@ void initParticles() {
 
 void ottalkivanie_dvuh(Particle& particle_1, Particle& particle_2) {
 
-// uprugo: delta_p = 2 x delta_V_norm; delta_V_norm = n_vector x delta_V_vector;
-// delta_V_vector = ( V1_vector - V_2_vector ) = ( V_1_x - V_2_x + V_1_y - V_2_y );
-// n_vector = n_x*i + n_y*j;
-// n_x(or y) = dx(or y) / distance_between_centers;
-// delta_V_norm = n_x*(V_1_x - V_2_x) + n_y*(V_1_y - V_2_y);
-// togda delta_p = 2 x [ n_x*(V_1_x - V_2_x) + n_y*(V_1_y - V_2_y) ];
-// V_1_x_final = V_1_x + delta_p x n_x, V_1_y_final = V_1_y + delta_p x n_y;
-// V_2_x_final = V_2_x - delta_p x n_x, V_2_y_final = V_2_y - delta_p x n_y;
-            
-            // rasstoyaniye
-    float dx = particle_2.x - particle_1.x;
-    float dy = particle_2.y - particle_1.y;
+    // uprugo: delta_p = 2 x delta_V_norm; delta_V_norm = n_vector x delta_V_vector;
+    // delta_V_vector = ( V1_vector - V_2_vector ) = ( V_1_x - V_2_x + V_1_y - V_2_y );
+    // n_vector = n_x*i + n_y*j;
+    // n_x(or y) = dx(or y) / distance_between_centers;
+    // delta_V_norm = n_x*(V_1_x - V_2_x) + n_y*(V_1_y - V_2_y);
+    // togda delta_p = 2 x [ n_x*(V_1_x - V_2_x) + n_y*(V_1_y - V_2_y) ];
+    // V_1_x_final = V_1_x + delta_p x n_x, V_1_y_final = V_1_y + delta_p x n_y;
+    // V_2_x_final = V_2_x - delta_p x n_x, V_2_y_final = V_2_y - delta_p x n_y;
+
+        // rasstoyaniye
+    float dx = particle_1.x - particle_2.x;
+    float dy = particle_1.y - particle_2.y;
 
     float distance_between_centers = sqrtf(dx * dx + dy * dy);
 
@@ -49,36 +50,53 @@ void ottalkivanie_dvuh(Particle& particle_1, Particle& particle_2) {
 
     //float delta_p = 2.0 * (n_x * (particle_1.vx - particle_2.vx) + n_y * (particle_1.vy - particle_2.vy));
 
-    float delta_p;
-    float delta_V_norm;
+    ///float delta_p;
+    //float delta_V_norm;
 
-    float delta_V_norm_x = n_x * (particle_1.vx - particle_2.vx);
-    float delta_V_norm_y = n_y * (particle_1.vy - particle_2.vy);
+    float delta_V_norm_x = (particle_1.vx - particle_2.vx);
+    float delta_V_norm_y = (particle_1.vy - particle_2.vy);
 
-    delta_V_norm = delta_V_norm_x + delta_V_norm_y;
-    delta_p = 2.0 * delta_V_norm;
-
-    
+    float delta_V_norm = n_x*delta_V_norm_x + n_y*delta_V_norm_y;
+    //delta_p = delta_V_norm;
 
 
-    particle_1.vx += n_x * delta_p;
-    particle_1.vy += n_y * delta_p;
-    particle_2.vx -= n_x * delta_p;
-    particle_2.vy -= n_y * delta_p;
+
+    //float _vremennaya_v = particle_1.vx;
+
+    if (delta_V_norm < 0) {
+
+        float imp = -2.0f * delta_V_norm;
+
+        particle_1.vx += imp * n_x;
+        particle_1.vy += imp * n_y;
+        particle_2.vx -= imp * n_x;
+        particle_2.vy -= imp * n_y;
+    }
 
 }
 
 
 void many_ootalkivaniya() {
+
+
     for (size_t i = 0; i < particles.size(); ++i) {
         for (size_t j = i + 1; j < particles.size(); ++j) {
-            
+
             Particle& particle_1 = particles[i];
             Particle& particle_2 = particles[j];
 
+            float min_distance = 0.05f;
+
             if (particle_1.type == particle_2.type) continue;
 
-            ottalkivanie_dvuh(particle_1, particle_2);
+            float dx = particle_1.x - particle_2.x;
+            float dy = particle_1.y - particle_2.y;
+
+            float distance = sqrtf(dx * dx + dy * dy);
+
+            if (distance < min_distance) {
+                ottalkivanie_dvuh(particle_1, particle_2);
+            }
         }
     }
 }
@@ -86,7 +104,7 @@ void many_ootalkivaniya() {
 
 // +coord x +coord y, proverka na predely ekrana
 void updateParticles() {
-    
+
     // ot kraev
     for (auto& p : particles) {
         p.x += p.vx;
@@ -104,7 +122,7 @@ void updateParticles() {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glPointSize(10.0f);
+    glPointSize(40.0f);
     glBegin(GL_POINTS);
 
     for (const auto& p : particles) {
@@ -114,8 +132,12 @@ void display() {
             glColor3f(1.0f, 0.0f, 0.0f);
         }
         else glColor3f(0.0f, 0.0f, 1.0f);
+
+        glVertex2f(p.x, p.y);
     }
-    
+
+
+
     glEnd();
 
     glutSwapBuffers();
@@ -131,12 +153,11 @@ void timer(int) {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitWindowSize(512, 512);
+    glutInitWindowSize(720, 720);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutCreateWindow("Particle System");
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
     initParticles();
 
     glutDisplayFunc(display);
